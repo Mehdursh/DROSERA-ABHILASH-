a15680263e451e7b42bedd62d8dd3c27dc73ed9d
Mehdursh <meshramsaurab92@gmail.com>
2025-04-20 17:15:41 +0530

    Update start node .sh

--- start node .sh
+++ start node .sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # अपने RPC और Private Key यहां भरो
-ETH_RPC_URL="https://your-rpc-url"
-ETH_PRIVATE_KEY="0xYOUR_PRIVATE_KEY"
+ETH_RPC_URL="https://ethereum-holesky-rpc.publicnode.com"
+ETH_PRIVATE_KEY="0x015609e4d6e287e6ae3d6d4c2155e8c077d431aa402b5af769a4543cd657b2df"
 
 drosera-operator node \
   --eth-rpc-url $ETH_RPC_URL \
